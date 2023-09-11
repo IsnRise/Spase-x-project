53ed6a8358d32f477f60067c602d96a4f3a10746
IsnRise <127597148+IsnRise@users.noreply.github.com>
2023-09-11 19:56:03 +0300

    Update download_nasa_epic_pictures.py

--- download_nasa_epic_pictures.py
+++ download_nasa_epic_pictures.py
@@ -33,6 +33,7 @@ def main():
     args = parser.parse_args()
     args_path = args.path
     Path(args_path).mkdir(parents=True, exist_ok=True)
+    fetch_nasa_epic_pictures(nasa_epic_url,nasa_token)
 
 if __name__ == "__main__":
     main()
